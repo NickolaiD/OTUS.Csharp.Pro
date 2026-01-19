da10ddc8bed4e2cbf0d3f1c137ab31b4469cf2bc
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-01-19 21:30:17 +0300

    HomeWork03

--- HomeWork03/FileReader/FileReader/Program.cs
+++ HomeWork03/FileReader/FileReader/Program.cs
@@ -1,12 +1,24 @@
-﻿namespace FileReader
+﻿using System.Diagnostics;
+
+namespace FileReader
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            var fileList = Directory.GetFiles(Path.Combine("D:", "files"));
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var spaceCount = GetSpaceCountFromFilesInFolder(Path.Combine("D:", "files"));
+            sw.Stop();
+            Console.WriteLine($"Общее количество пробелов: {spaceCount}");
+            Console.WriteLine($"Время выполнения (миллисекунд): {sw.ElapsedMilliseconds}");
+        }
 
-            var  tasks = new List<Task<int>>();
+        static int GetSpaceCountFromFilesInFolder(string filePath)
+        {
+            var fileList = Directory.GetFiles(filePath);
+
+            var tasks = new List<Task<int>>();
             foreach (var file in fileList)
             {
                 tasks.Add(Task<int>.Run(() => GetSpaceCountFromFile(file)));
@@ -19,9 +31,9 @@
                 totalSpaceCount += task.Result;
             }
 
-            Console.WriteLine($"Общее количество пробелов: {totalSpaceCount}");
+            return totalSpaceCount;
         }
-
+        
         static int GetSpaceCountFromFile(string fileName)
         {
             var text = File.ReadAllText(fileName);
