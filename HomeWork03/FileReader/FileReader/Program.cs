42efe487af27e08c08b36fff87ba0ad62f66ba50
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-01-19 21:17:40 +0300

    HomeWork03

---
+++ HomeWork03/FileReader/FileReader/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FileReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileList = Directory.GetFiles(Path.Combine("D:", "files"));
+
+            var  tasks = new List<Task<int>>();
+            foreach (var file in fileList)
+            {
+                tasks.Add(Task<int>.Run(() => GetSpaceCountFromFile(file)));
+            }
+            Task.WaitAll(tasks);
+
+            var totalSpaceCount = 0;
+            foreach (var task in tasks)
+            {
+                totalSpaceCount += task.Result;
+            }
+
+            Console.WriteLine($"Общее количество пробелов: {totalSpaceCount}");
+        }
+
+        static int GetSpaceCountFromFile(string fileName)
+        {
+            var text = File.ReadAllText(fileName);
+            return text.Where(x => x.Equals(' ')).Count();
+        }
+    }
+}
