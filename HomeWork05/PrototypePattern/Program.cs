6f772562374544103174e6ab62b3107f2e6b6a32
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-03-07 16:23:19 +0300

    HomeWork05

--- HomeWork05/PrototypePattern/Program.cs
+++ HomeWork05/PrototypePattern/Program.cs
@@ -7,8 +7,8 @@
             var triangle = new Triangle("red", 1, 2, 3, 4, 5);
             triangle.Draw();
 
-            var triangle1 = triangle.MyClone();
-            Console.WriteLine("MyClone " + triangle1.GetInfo());  //приведение типа не требуется
+            var triangle1 = triangle.MyClone();  //приведение типа не требуется
+            Console.WriteLine("MyClone " + triangle1.GetInfo());  
 
             var triangle2 = triangle.Clone();
             Console.WriteLine("Clone " + ((Triangle)triangle2).GetInfo());
@@ -17,8 +17,8 @@
             var circle = new Circle("black", 10, 20, 30);
             circle.Draw();
 
-            var circle1 = circle.MyClone();
-            Console.WriteLine("MyClone " + circle1.GetInfo());  //приведение типа не требуется
+            var circle1 = circle.MyClone();  //приведение типа не требуется
+            Console.WriteLine("MyClone " + circle1.GetInfo());  
 
             var circle2 = circle.Clone();
             Console.WriteLine("Clone " + ((Circle)circle2).GetInfo());
