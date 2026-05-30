97e14eff9d1a394ed47d0e55b9d1b1516d5cef9c
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-05-30 21:05:11 +0300

    HomeWork07

---
+++ HomeWork07/Reflection/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f = F.Get();
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            string fSerialized = string.Empty;
+            for (int x = 1; x < 100000; x++)
+            {
+                fSerialized = Serializer.Serialize(f);
+            }
+
+            stopWatch.Stop();
+            Console.WriteLine("Собственная реализация");
+            Console.WriteLine(fSerialized);
+            Console.WriteLine($"Время выполнения: {stopWatch.ElapsedMilliseconds}");
+
+            stopWatch.Restart();
+            stopWatch.Start();
+                        
+            for (int x = 1; x < 100000; x++)
+            {
+                fSerialized = JsonSerializer.Serialize(f);
+            }
+
+            stopWatch.Stop();
+
+            Console.WriteLine("\nJson Serializer");
+            Console.WriteLine(fSerialized);
+            Console.WriteLine($"Время выполнения: {stopWatch.ElapsedMilliseconds}");
+
+            string csvLine = "10,20,30,40,50";
+            var person = Serializer.Deserialize<F>(csvLine);
+
+
+            stopWatch.Restart();
+            stopWatch.Start();
+            for (int x = 1; x < 100000; x++)
+            {
+                var persons = LoadFromFile<F>(Path.Combine("d:", "data.csv"));
+            }
+            stopWatch.Stop();
+            
+            
+            Console.WriteLine($"\nСериализация из файла, время выполнения: {stopWatch.ElapsedMilliseconds}");
+
+            Console.ReadKey();
+        }
+
+        public static List<T> LoadFromFile<T>(string filePath, char delimiter = ',') where T : new()
+        {
+            List<T> result = new List<T>();
+            string[] lines = File.ReadAllLines(filePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (!string.IsNullOrWhiteSpace(lines[i]))
+                {
+                    result.Add(Serializer.Deserialize<T>(lines[i], delimiter));
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HomeWork07/Reflection/Serializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Reflection
+{
+    public static class Serializer
+    {
+        public static string Serialize(object obj)
+        {
+            if (obj == null)
+                throw new ArgumentNullException(nameof(obj));
+
+            StringBuilder result = new StringBuilder();
+            Type type = obj.GetType();
+
+            result.AppendLine($"{type.Name}:");
+            // Сериализация свойств
+            PropertyInfo[] properties = type.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var property in properties)
+            {
+
+                // Проверяем, есть ли геттер
+                MethodInfo? getMethod = property.GetGetMethod(true);
+                if (getMethod != null)
+                {
+                    object? value = property.GetValue(obj);
+                    result.AppendLine($"  {property.Name} ({property.PropertyType.Name}) = {(value == null ? "null" : value)}");
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static T Deserialize<T>(string csvLine, char delimiter = ',') where T : new()
+        {
+            string[] values = csvLine.Split(delimiter);
+            T obj = new T();
+
+            PropertyInfo[] properties = typeof(T).GetProperties();
+
+            for (int i = 0; i < Math.Min(properties.Length, values.Length); i++)
+            {
+                if (!string.IsNullOrEmpty(values[i]))
+                {
+                    try
+                    {
+                        object convertedValue = Convert.ChangeType(values[i], properties[i].PropertyType);
+                        properties[i].SetValue(obj, convertedValue);
+                    }
+                    catch
+                    {
+                        // Пропускаем ошибки конвертации
+                    }
+                }
+            }
+
+            return obj;
+        }
+    }
+}
