c812d6e1b1172fdee07a1f7372f03e1edcec0ce0
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-05-31 22:07:31 +0300

    HomeWork06

---
+++ HomeWork06/SOLIDExample/GameUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLIDExample
+{
+    public class GameUI
+    {
+        public int GetNumberFromUser()
+        {
+            while (true)
+            {
+                Console.Write("Введите число: ");
+
+                if (int.TryParse(Console.ReadLine(), out int number))
+                    return number;
+
+                Console.WriteLine("Ошибка! Повторите ввод числа");
+            }
+        }
+    }
+}
