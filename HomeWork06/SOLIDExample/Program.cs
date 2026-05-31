c13c108365a5feab8a49cdb26099567fd00bf501
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-05-31 19:17:10 +0300

    HomeWorlk06

--- HomeWork06/SOLIDExample/Program.cs
+++ HomeWork06/SOLIDExample/Program.cs
@@ -4,17 +4,8 @@
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-            var k = random.Next(0, 100);
-            int x = -1;
-            do
-            {
-                x = int.Parse(Console.ReadLine());
-                if (x < k)
-                    Console.WriteLine("Больше");
-                if (x > k)
-                    Console.WriteLine("Меньше");
-            } while (x != k);
+            var game = new Game(0, 100, 10);
+            game.Start();
             
         }
     }
