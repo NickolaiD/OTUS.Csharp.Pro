0ef0c83825984299565baf0a7f440b9cc1f4bd84
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-06-13 13:13:11 +0300

    HomeWork06

---
+++ HomeWork06/SOLIDExample/SRP/GameLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLIDExample
+{
+    public class GameLogic
+    {
+        public Result Check(int userInput, int numberToCheck)
+        {
+            if (userInput < numberToCheck)
+                return(Result.upper);
+            if (userInput > numberToCheck)
+                return (Result.lower);
+            return (Result.win);
+
+        }
+    }
+}
