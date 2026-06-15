9629be8aa9753956094c3664426326a67ea82155
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-06-15 16:43:07 +0300

    HomeWork08

---
+++ HomeWork08/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
