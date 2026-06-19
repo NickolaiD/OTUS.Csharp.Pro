616c1d008da82bcfaaaeb42906f553e925f8bb8a
Nikolay Drozdov <nickolai.drozdow@gmail.com>
2026-06-19 16:54:19 +0300

    HomeWork08

---
+++ HomeWork08/EnumerableExtension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace HomeWork08
+{
+    public static class EnumerableExtension
+    {
+        public static T? GetMax<T>(this IEnumerable collection, Func<T, float> convertToNumber) where T : class
+        {
+            if (collection == null)
+                throw new ArgumentNullException(nameof(collection));
+
+            if (convertToNumber == null)
+                throw new ArgumentNullException(nameof(convertToNumber));
+
+            T? maxElement = null;
+            float maxValue = float.MinValue;
+            bool hasElements = false;
+
+            foreach (object item in collection)
+            {
+                if (item is T typedItem)
+                {
+                    float currentValue = convertToNumber(typedItem);
+
+                    if (!hasElements || currentValue > maxValue)
+                    {
+                        maxValue = currentValue;
+                        maxElement = typedItem;
+                        hasElements = true;
+                    }
+                }
+            }
+
+            if (!hasElements)
+                throw new InvalidOperationException("Коллекция не содержит элементов типа T или пуста");
+
+            return maxElement;
+        }
+    }
+}
--- HomeWork08/Program.cs
+++ HomeWork08/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        
+            var people = new List<Person>
+            {
+                new Person { Name = "Анна", Age = 25 },
+                new Person { Name = "Борис", Age = 30 },
+                new Person { Name = "Виктор", Age = 22 },
+                new Person { Name = "Галина", Age = 35 }
+            };
+
+            var oldest = people.GetMax<Person>(p => p.Age);
+            if (oldest != null)
+            {
+                Console.WriteLine($"Самый старший: {oldest.Name} {oldest.Age}");
+            }
+
         }
     }
 }
